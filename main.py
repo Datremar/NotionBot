9f2755e2a2852b75ab65ebd808b4477a18970386
Datremar <mr.protoss@bk.ru>
2023-06-30 15:51:50 +0700

    Forgot to include .log file creation

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tg_bot.utils.cache import cache
 from config import config
 
 logging.basicConfig(
-    # filename="logs.log",
+    filename="logs.log",
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO if config["logger"]["debug"] == False else logging.DEBUG,
     encoding="utf-8"
